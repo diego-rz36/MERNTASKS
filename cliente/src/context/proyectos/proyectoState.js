b62c45789c13ebc2c07e2660208e00d15b36ea69
Diego <diegorz36@gmail.com>
2020-07-09 20:01:43 -0500

    ultimo

--- cliente/src/context/proyectos/proyectoState.js
+++ cliente/src/context/proyectos/proyectoState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 
-import uuid from 'uuid';
+//import uuid from 'uuid';
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
 import { 
@@ -12,6 +12,7 @@ import {
     ELIMINAR_PROYECTO
  } from '../../types';
 
+ import clienteAxios from '../../config/axios';
 
 
 const ProyectoState = props => {
@@ -47,14 +48,21 @@ const ProyectoState = props => {
         });
     }
     //Agregar un nuevo proyecto
-    const agregarProyecto = proyecto => {
-        proyecto.id = uuid.v4();
+    const agregarProyecto = async proyecto => {
+        //proyecto.id = uuid.v4();
 
         //Insertar el proyecto en el state
-        dispatch({
-            type: AGREGAR_PROYECTO,
-            payload: proyecto
-        });
+        
+        try {
+            const resultado = await clienteAxios.post('/api/proyectos', proyecto);
+            console.log(resultado);
+            dispatch({
+                type: AGREGAR_PROYECTO,
+                payload: resultado.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     //Valida el Formulario
